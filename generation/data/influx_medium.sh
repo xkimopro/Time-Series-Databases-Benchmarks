f3df9a0d07da7faec6ef03a22d7d6b7cf0fc61c7
xkimopro <kimonaspro99@gmail.com>
2022-01-07 23:51:23 +0200

    Fixes

--- generation/data/influx_medium.sh
+++ generation/data/influx_medium.sh
@@ -1,5 +1,5 @@
 tsbs_generate_data --use-case="iot" --seed=123 --scale=4000 \
     --timestamp-start="2016-01-01T00:00:00Z" \
-    --timestamp-end="2016-01-05T01:00:00Z" \
+    --timestamp-end="2016-01-05T04:00:00Z" \
     --interleaved-generation-groups=10 \
     --log-interval="10s" --format="influx" > ~/iot_data/influx-data-medium.csv
