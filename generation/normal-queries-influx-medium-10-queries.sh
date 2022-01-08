edc58f9c295a5660a0048dac7b7f67317f748159
Luke <el15715@central.ntua.gr>
2022-01-08 11:02:16 +0200

    queries

---
+++ generation/normal-queries-influx-medium-10-queries.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+
+FORMATS="influx" SCALE=4000 SEED=123     TS_START="2016-01-01T00:00:00Z"     TS_END="2016-01-05T04:00:01Z"     QUERIES=10  USE_CASE="iot"  BULK_DATA_DIR="queries/nomral-queries/influxdb/medium/10-queries" ./generate_queries_lkh.sh
---
+++ generation/queries/cold-start/influxdb/small/influx-queries-cold-start-long-daily-sessions-small-2-queries.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+var=`echo $PWD`
+num=${var::-33}
+num+="":$PATH""
+export PATH=$num
+
+tsbs_generate_queries --use-case="iot" --seed=123 --scale=4000 \
+    --timestamp-start="2016-01-01T00:00:00Z" \
+    --timestamp-end="2016-01-01T10:00:01Z" \
+    --queries=2 --query-type="long-daily-sessions" --format="influx" \
+     > /home/luke-v2/Desktop/queries/cold-start/influxdb/small/influx-queries-cold-start-long-daily-sessions-small-2-queries.flux
